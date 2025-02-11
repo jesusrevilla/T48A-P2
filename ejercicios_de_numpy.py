f8ee447a311c69838a753de1a0bbf8b47bbd8e5e
JackRGMa <145623130+JackRGMa@users.noreply.github.com>
2025-02-10 19:49:43 -0600

    Update ejercicios_de_numpy.py

--- ejercicios_de_numpy.py
+++ ejercicios_de_numpy.py
@@ -450,7 +450,7 @@ def multiply_broadcast(arr1, arr2):
   arr2: numpy.ndarray
     arreglo de numpy de forma (3, 2).
   '''
-  return arr1 * arr2
+  return arr1 * arr2.T
 
 multiply_broadcast(np.array([[1, 2, 3], [4, 5, 6]]), np.array([[1, 2], [3, 4], [5, 6]]))
 
