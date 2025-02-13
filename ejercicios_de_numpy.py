123347d4723b3685cb4808cd4314f9b428279b4d
Blaster Sun <135788175+BlasterSun10@users.noreply.github.com>
2025-02-12 19:45:14 -0600

    Update ejercicios_de_numpy.py
    v4

--- ejercicios_de_numpy.py
+++ ejercicios_de_numpy.py
@@ -580,7 +580,7 @@ def rainfall_data(rainfall):
   rainfall: numpy.ndarray
     arreglo 2D de numpy de lluvia en mm y ciudades.
   '''
-  print("Índices de las ciudades con más de 100 mm de lluvia:", np.where(rainfall > 100)[0]
+  print("Índices de las ciudades con más de 100 mm de lluvia:", np.where(rainfall > 100)[0])
 
 
 """3. Image Thresholding:  Imagine a grayscale image represented as a 2D NumPy array.  Create a mask to select pixels with intensity values greater than a certain threshold (e.g., 128).  Set the values of these pixels to 255 (white) and the remaining pixels to 0 (black). This simulates a simple image thresholding operation."""
