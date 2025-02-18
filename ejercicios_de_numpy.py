e2357ed8013a266f0b0a334e9507b04b77310245
Jose Emanuel Guerrero Torres <134025722+melman77@users.noreply.github.com>
2025-02-17 19:33:21 -0600

    176308

--- ejercicios_de_numpy.py
+++ ejercicios_de_numpy.py
@@ -549,6 +549,8 @@ def rainfall_data(rainfall):
   '''
   print("Índices de las ciudades con más de 100 mm de lluvia:", np.where(rainfall > 100)[0])
 
+
+
 """3. Image Thresholding:  Imagine a grayscale image represented as a 2D NumPy array.  Create a mask to select pixels with intensity values greater than a certain threshold (e.g., 128).  Set the values of these pixels to 255 (white) and the remaining pixels to 0 (black). This simulates a simple image thresholding operation."""
 
 import numpy as np
@@ -585,5 +587,7 @@ def matrix_diagonals(matrix):
     - matrix.shape == (5, 5)
   '''
   return np.diagonal(matrix), np.fliplr(matrix).diagonal()
+
+
 
 """#Text"""
